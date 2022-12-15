678cd84c884844a5c4843afe472d5531ec21127f
Emily Bradfield <emily-bradfield@outlook.com>
2022-12-15 15:21:14 +0000

    code go skrrt

---
+++ v_two.py
@@ -0,0 +1,30 @@
+# Import the necessary libraries
+import spacy
+import random
+from openai import api_key, Model
+
+# Load the pre-trained NLP model
+nlp = spacy.load('en_core_web_sm')
+
+# Load the pre-trained NLG model
+model = Model.get_model('text-davinci-002')
+
+while True:
+    # Prompt the user to enter their message
+    user_input = input('Enter your message: ')
+
+    # Use the NLP model to analyze and interpret the user's input
+    doc = nlp(user_input)
+
+    # Generate a response to the user's input using the NLG model
+    response = model.generate(
+        prompt=f'Hilly is a wonderful person! What do you think about that?',
+        max_tokens=256,
+        temperature=0.5,
+        top_p=1.0,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Print the generated response
+    print(response)
